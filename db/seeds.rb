fdee32946f18624dff5c5ece9790ea3d0022521a
Mackenzie Rempel <mackenziegrempel@gmail.com>
2020-10-01 17:51:23 -0500

    Added seed script

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# Adds products to the db.
+
+676.times do
+  Product.create(title:          Faker::Commerce.unique.product_name,
+                 price:          Faker::Commerce.price,
+                 stock_quantity: Faker::Number.decimal)
+end
