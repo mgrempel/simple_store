dcd1655ba24d3fda86c98da0e62a36f87bc190cb
Mackenzie Rempel <mackenziegrempel@gmail.com>
2020-10-03 20:47:34 -0500

    Updated seeds script

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-# Adds products to the db.
+require "csv"
 
-676.times do
-  Product.create(title:          Faker::Commerce.unique.product_name,
-                 price:          Faker::Commerce.price,
-                 stock_quantity: Faker::Number.decimal)
+# Clear out the db
+Product.destroy_all
+Category.destroy_all
+
+# seed db
+csv_file = Rails.root.join("db/products.csv")
+csv_data = File.read(csv_file)
+
+products = CSV.parse(csv_data, headers: true)
+
+products.each do |product|
+  # Check if category exists, if not, create it
+  category = if Category.exists?(name: product["category"])
+               Category.where(name: product["category"]).first
+             else
+               Category.create(name: product["category"])
+             end
+
+  category.products.build(title:          product["name"],
+                          description:    product["description"],
+                          price:          product["price"],
+                          stock_quantity: product["stock quantity"])
+  category.save
 end
